779826b815c5709e0e05cc1a70e9f846a662cd60
Joseph-Moheb-2018 <36422640+Joseph-Moheb-2018@users.noreply.github.com>
2020-06-01 15:02:28 +0200

    Add files via upload

--- Main/LLA/prediction/views.py
+++ Main/LLA/prediction/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
-
+from analysis.models import peaple
+from django.http import HttpResponse
+import pandas as pd
 
 def prediction(request):
-    return render(request, "prediction/prediction.html")
+    return render(request, "prediction/prediction.html",
+                  {"subjects": peaple.objects.all()})
 
 
 def upload(request):
     if request.method == 'POST':
-        uploaded_file = request.files['document']
-        fs = FileSystemStorage
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
-    return render(request, 'prediction.html')
+        data_in = pd.read_csv("media/" + uploaded_file.name)
+        return HttpResponse(data_in.to_html())
+
+    return render(request, 'prediction/prediction.html',
+                  {"subjects": peaple.objects.all()})
+
