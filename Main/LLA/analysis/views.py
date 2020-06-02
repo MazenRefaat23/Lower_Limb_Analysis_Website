023f297b986978d936d7c61fcc87290ec3f9f1b3
MahmoudHafez1 <36639602+MahmoudHafez1@users.noreply.github.com>
2020-06-02 17:38:23 +0200

    Update views.py

--- Main/LLA/analysis/views.py
+++ Main/LLA/analysis/views.py
@@ -6,7 +6,7 @@ from . import data_analysis
 
 def person(request, id):
     subject = get_object_or_404(peaple, pk=id)
-    data_out1,data_out2=data_analysis.analysis(subject.name)
+    largedata,data_out1,data_out2=data_analysis.analysis()
     grd = data_out1.loc[data_out1['Activity'] == 'Level ground walking']
     asc = data_out1.loc[data_out1['Activity'] == 'Ramp ascent']
     des = data_out1.loc[data_out1['Activity'] == 'Ramp descent']
@@ -19,8 +19,12 @@ def person(request, id):
     grd_time = list(grd.Stride_time)
     asc_time = list(asc.Stride_time)
     des_time = list(des.Stride_time)
-    largedata = data_analysis.large_list
     data_out2['Calories'] = [0,0,0,0,0]
+    mean_val = [data_out1['Stride_length'].mean(), data_out1['Speed'].mean()]
+    mas_ = list(data_out1.msa)
+    mas_1 = [x for x in mas_ if x == x]
+    mast_ = list(data_out1.mast)
+    mast_2 = [x for x in mast_ if x == x]
     describe_Stride_length = data_out1.groupby('Activity')['Stride_length'].describe().to_html()
     describe_Speed = data_out1.groupby('Activity')['Speed'].describe().to_html()
 
@@ -28,10 +32,11 @@ def person(request, id):
                   {"sub": subject,'grd_sl':grd_sl,
                    'asc_sl':asc_sl,'des_sl':des_sl,
                    'grd_speed':grd_speed,'asc_speed':asc_speed,
-                   'des_speed':des_speed,'grd_time':grd_time,
+                   'des_speed':des_speed,'grd_time':grd_time,'large_data':largedata,
                    'asc_time':asc_time,'des_time':des_time,
-                   'large_data':largedata,
-                   'data_out2':data_out2,
+                   'data_out2':data_out2,'mas_1':mas_1,
+                   'mast_2':mast_2,
+                   'mean_val':mean_val,
                    'describe_Stride_length' : describe_Stride_length,
                    'describe_Speed':describe_Speed,
                    "subjects": peaple.objects.all()})
